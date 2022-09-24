4af900c4637991748e1aef5980201bcd045e4d23
Rigoberto Calderon <jesuscalflo15@gmail.com>
2022-09-24 18:51:00 -0500

    Agregando atributos y notaciones de validación,

--- src/main/java/com/rcalderon/form_app/models/Usuario.java
+++ src/main/java/com/rcalderon/form_app/models/Usuario.java
@@ -1,8 +1,14 @@
 package com.rcalderon.form_app.models;
 
+import java.util.Date;
+
 import javax.validation.constraints.Email;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import com.rcalderon.form_app.validators.IdentificadorRegex;
@@ -10,6 +16,8 @@ import com.rcalderon.form_app.validators.RequeridoValidator;
 
 public class Usuario {
 
+    /// @Past para dateformat, tiene que ser una fecha en el pasado
+    /// @Future tiene que ser una fecha en el futuro a partir de hoy
     /// Clase messages.properties es interpretado
     /// para sobrescribir los mensajes de error con las notaciones de @Validation
 
@@ -32,6 +40,34 @@ public class Usuario {
     @NotEmpty
     @Email(message = "Ingresa un email válido") // Válida que sea en formato email
     private String email;
+    // Para objetos, sirve esta validación
+    // _> int -> primitivo
+    @NotNull
+    @Min(5)
+    @Max(5000)
+    private Integer cuenta;
+
+    // @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Future
+    @NotNull
+    private Date birthday;
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Integer getCuenta() {
+        return this.cuenta;
+    }
+
+    public void setCuenta(Integer cuenta) {
+        this.cuenta = cuenta;
+    }
+
     // @NotEmpty
     @RequeridoValidator
     private String nombre;
